3f9a45fda13e3d25269565ef54202b921d7724d1
Kim Hwan <kimlove2324@gmail.com>
2019-08-25 17:34:13 +0900

    add navbar opacity

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-const app = document.getElementById('typeWriter');
+const app = document.getElementById("typeWriter");
+const ball = document.querySelector(".turtle");
 const scroll = new SmoothScroll('a[href*="#"]', {
     speed: 1500
 });
@@ -6,12 +7,20 @@ const typewriter = new Typewriter(app, {
     loop: true
 });
 
-typewriter.typeString('FrontEnd')
+typewriter
+    .typeString("FrontEnd")
     .pauseFor(2500)
     .deleteAll()
-    .typeString('BackEnd')
+    .typeString("BackEnd")
     .pauseFor(2500)
     .deleteChars(7)
-    .typeString('FullStack')
+    .typeString("FullStack")
     .pauseFor(2500)
     .start();
+
+const handleClick = e => {
+    ball.classList.add("showing");
+    ball.style.transform = `translate(${e.pageX}px, ${e.pageY}px)`;
+};
+
+document.addEventListener("mousemove", handleClick);

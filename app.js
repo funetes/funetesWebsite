10a49ca98d2d5fc910c99dc77130f6e419241dfb
Kim Hwan <kimlove2324@gmail.com>
2019-09-08 09:53:20 +0900

    add cocoaTalk code review in modal

--- app.js
+++ app.js
@@ -1,26 +1,16 @@
 const app = document.getElementById("typeWriter");
 const ball = document.querySelector(".turtle");
 const scroll = new SmoothScroll('a[href*="#"]', {
-    speed: 1500
+  speed: 1500
 });
 const typewriter = new Typewriter(app, {
-    loop: true
+  loop: true
 });
-
 typewriter
-    .typeString("FrontEnd")
-    .pauseFor(2500)
-    .deleteAll()
-    .typeString("BackEnd")
-    .pauseFor(2500)
-    .deleteChars(7)
-    .typeString("FullStack")
-    .pauseFor(2500)
-    .start();
-
-const handleClick = e => {
-    ball.classList.add("showing");
-    ball.style.transform = `translate(${e.pageX}px, ${e.pageY}px)`;
-};
-
-document.addEventListener("mousemove", handleClick);
+  .typeString("Developer")
+  .pauseFor(2500)
+  .deleteAll()
+  .typeString("Programmer")
+  .pauseFor(2500)
+  .deleteAll()
+  .start();
